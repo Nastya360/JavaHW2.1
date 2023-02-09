87c414d60e5efd3237974f8237c336652a3d6ccf
Neto test <anastasiya.parfinenko@gmail.com>
2023-02-09 16:31:06 +0600

    firstcommit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 8000;
+        int bonusRuble = 20;
+        int mile = (amount/bonusRuble);
+
+        System.out.println("Начиленно миль:"+mile);
+
+
+    }
+}
